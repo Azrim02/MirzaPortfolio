2c417c3e2519db4328b4074053e3a4587f2e08ae
Azrim02 <mirzashariman02@gmail.com>
2025-11-27 23:55:39 +0800

    init portfolio

---
+++ src/Playground.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+
+function Playground() {
+  return (
+    <div className="Playground">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default Playground;
