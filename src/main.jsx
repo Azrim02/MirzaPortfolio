e2e9053dc7a62b1983b8323641026adfd850da1b
Azrim02 <mirzashariman02@gmail.com>
2025-11-28 15:55:56 +0800

    folder restructuring

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './App.jsx'
-import Playground from './Playground.jsx'
+import Playground from './pages/Playground.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
