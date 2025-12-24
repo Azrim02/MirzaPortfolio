625230c08139efa1be75c0dbd912d1530eb6ef3e
Azrim02 <mirzashariman02@gmail.com>
2025-12-24 22:42:37 +0800

    added button component

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import '../styles/App.css'
+/*import '../styles/App.css'*/
+import Button from '../components/Button';
+import '../styles/colors.scss'
 
 function Playground() {
   return (
     <div className="Playground">
-      <h1>Hello World!</h1>
+      <h1>Hello World! <br />Yayangku COMEYYYY xD </h1>
+      <Button>Default</Button>
+
+      <Button variant="confirm">
+        Confirm
+      </Button>
+
+      <Button variant="delete">
+        Delete
+      </Button>
+      <br></br>
+      <Button variant="borderless">
+        Cancel
+      </Button>
+
+      <Button variant="confirm" loading>
+        Saving
+      </Button>
+
+      <Button variant="delete" disabled>
+        Delete
+      </Button>
+      
+
     </div>
   );
 }
