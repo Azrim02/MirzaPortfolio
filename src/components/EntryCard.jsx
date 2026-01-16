90e4f026a602ae00e165eadad64f38cce8025037
Azrim02 <mirzashariman02@gmail.com>
2026-01-16 13:01:49 +0800

    init EntryCard component
    EntryCard receives 4 arguments; title, description, data, fileName

---
+++ src/components/EntryCard.jsx
@@ -0,0 +1,19 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function EntryCard({ title, description, date, fileName }) {
+  return (
+    <Card className="text-center">
+      {/* <Card.Header>{title || 'Untitled'}</Card.Header> */}
+      <Card.Body>
+        <Card.Title>{title || 'Untitled'}</Card.Title>
+        <Card.Text>
+          {description || 'No description available.'}
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">{date || fileName}</Card.Footer>
+    </Card>
+  );
+}
+
+export default EntryCard;
