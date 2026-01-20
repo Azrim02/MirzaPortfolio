ceff63d191a2bde3af5dfffb511d4b24eef82b98
Azrim02 <mirzashariman02@gmail.com>
2026-01-20 23:16:17 +0800

    Updates

---
+++ assets/initWebsite-48aFTQYq.js
@@ -0,0 +1,27 @@
+const e=`---
+date: 20-01-26
+project: Websites
+tags:
+  - WebDev
+  - React
+  - Markdown
+---
+# My own portfolio website!
+
+Hello! It's a great honour to have you reading this as one of my earliest visitors. I hope you'll have a great time looking around on what I've been and will be up to. Without further ado... 
+
+Introducing my portfolio website! It's finally looking well! Though there are some things I need to set up before officially announcing it in my LinkedIn ( setting domain names, hosting, further stylizing, bla bla blahh...), I'd figured that this will be a perfect opportunity to write what I've done for the past few days before forgetting them in a few months time.
+
+The website leverages on React.js front-end web development framework, with Vite as its build configuration tool. Pretty simple, I know, but it fits the purpose I was going for for the website. Several components and external libraries that are available online were used in this project, as I wouldn't want to be reinventing the wheel when somebody else had already done the hard work implementing them. The Home page turns out pretty good, and I'm proud of it. :D
+
+![[Pasted image 20260120230429.png]]
+
+As you noticed, I'll be posting a number of entries on this webpage, which are stored in the form of markdown files. These files are usually created, edited, and enriched in Obsidian, which is a note-taking app I've used diligently over the past few months. To consistently edit my entries in html on vscode would be discouraging at best in the long term, hence integrating my existing workflow in Obsidian to be presented in the website was quite a unique solution I came with (thanks Claude Haiku for realizing this). 
+
+Each of these entries might have a project associated with them. In the Projects page, you'll be able to see entries that are related to the said project, and I'll be making more and more weird stuffs from now on. Stay tuned!
+
+Anddd... that's it folks. See you on the next entries to come!
+
+
+
+`;export{e as default};
--- assets/portfolio2-BLhiPiRf.js
+++
@@ -1,9 +0,0 @@
-const e=`---
-date: "19-01-26"
-project: "Portfolio Website"
-tags: ["Web Development", "React"]
----
-
-# WEBSITE WORKSSS
-
-asihdhasihdawdha`;export{e as default};
