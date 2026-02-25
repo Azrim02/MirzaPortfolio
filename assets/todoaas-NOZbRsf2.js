c0c000b98ace1576afcd56f75646cf8476a6b099
Azrim02 <mirzashariman02@gmail.com>
2026-02-25 20:20:23 +0800

    Updates

---
+++ assets/todoaas-NOZbRsf2.js
@@ -0,0 +1,15 @@
+const e=`---
+date: 20-02-26
+project: Web Development
+tags:
+  - Express.js
+  - Restful APIs
+  - Test-Driven Development
+---
+# Todo-as-a-service
+
+Most of the part you see on a website are the 'front-end' components. I'm sure you've heard of the 'back-end' part that works in the shadow... but what does it actually do? 
+
+WIP, to be written :P
+- RESTful APIs
+- Test-driven development`;export{e as default};
